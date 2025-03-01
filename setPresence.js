8aea54362bbcd0183ec1273f72fd142a87145590
tomato <104347100+tomeitoo@users.noreply.github.com>
2025-03-01 15:32:37 +0900

    Update setPresence.js

--- setPresence.js
+++ setPresence.js
@@ -1,9 +1,8 @@
-// setPresence.js
 module.exports = function(client) {
     client.user.setPresence({
         status: 'online',
         activities: [{ name: 'サポート中', type: 'PLAYING' }]
-    })
-    console.log("Presence updated!"))
-    .catch(console.error);
+    }).then(() => {
+        console.log("Presence updated!");
+    }).catch(console.error); 
 };
