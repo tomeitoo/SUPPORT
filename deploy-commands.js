7d4fc423585eed363f4dd5eb36c3811a0904737a
tomato <104347100+tomeitoo@users.noreply.github.com>
2025-04-14 18:29:23 +0900

    Update deploy-commands.js

--- deploy-commands.js
+++ deploy-commands.js
@@ -19,7 +19,7 @@ if (!TOKEN || !CLIENT_ID) {
 const commands = [
     {
         name: 'set_alert_role',
-        description: '警告を通知するロールを設定します。',
+        description: '警告を通知するロールを設定します',
         options: [{
             name: 'role',
             type: 8,
@@ -40,106 +40,71 @@ const commands = [
             {
                 name: 'reason',
                 type: 3,
-                description: '経費を申請する理由を教えてください。',
+                description: '理由を記入してください。',
                 required: true
             }
         ]
     },
     {
         name: 'set_treasury',
-        description: '金庫内の金額を設定します。',
+        description: '金庫の金額を設定します。',
         options: [{
             name: 'amount',
             type: 4,
-            description: '金庫へ追加する金額を入力してください。',
+            description: '金額を入力してください。',
             required: true
         }]
     },
     {
         name: 'add_treasury',
-        description: '金庫内に金額を追加します。',
+        description: '金庫に金額を追加します。',
         options: [{
             name: 'amount',
             type: 4,
-            description: '追加する金額を入力してください。',
+            description: '追加する金額を指定してください。',
             required: true
         }]
     },
     {
-        name: 'schedule_crime',
-        description: '犯罪を予約します。',
+        name: 'reset_crime',
+        description: 'リセット通知を出します。',
         options: [
             {
                 name: 'crime_name',
                 type: 3,
-                description: '犯罪の名前を入力してください。',
-                required: true
-            },
-            {
-                name: 'start_time',
-                type: 3,
-                description: '開始時間 (HH:MM)を入力してください。',
-                required: true
-            }
-        ]
-    },
-    {
-        name: 'attendance',
-        description: '出欠を登録します。',
-        options: [
-            {
-                name: 'status',
-                type: 3, // STRING
-                description: '参加 or 不参加',
+                description: '犯罪を選択してください。(コマンドは複数サーバーで共通です。サーバーにあったコマンドを使用してください。)',
                 required: true,
                 choices: [
-                    { name: '参加', value: '参加します。' },
-                    { name: '不参加', value: '不参加です。' }
+                    { name: 'コンビニ強盗', value: 'convenience_store_robbery' },
+                    { name: '金庫強盗', value: 'safecracker' },
+                    { name: '客船強盗', value: 'yacht robbery' },
+                    { name: 'オイルリグヘイスト', value: 'oil_rig_heist' },
+                    { name: '飛行場襲撃', value: 'airfield_assault' },
+                    { name: 'ユニオン銀行襲撃', value: 'union_robbery' },
+                    { name: 'ボブキャット襲撃', value: 'Bobcat_raid' },
+                    { name: 'アーティファクト', value: 'artifact' },
+                    { name: 'バージ', value: 'barge' },
                 ]
-            },
-            {
-                name: 'reason',
-                type: 3, 
-                description: '理由を入力してください',
-                required: false
             }
         ]
-    },
-    {
-        name: 'event',
-        description: 'イベントを作成します。',
-        options: [
-            {
-                name: 'title',
-                type: 3,
-                description: 'イベントのタイトルを入力してください。',
-                required: true
-            },
-            {
-                name: 'datetime',
-                type: 3,
-                description: '実施日時 (YYYY/MM/DD HH:MM)を入力してください。',
-                required:false 
-            },
-            {
-                name: 'deadline',
-                type: 3,
-                description: '募集締切時間 (HH:MM)を入力してください。',
-                required: true
-            },
-            {
-                name: 'description',
-                type: 3,
-                description: 'イベントの詳細を入力してください。',
-                required: false
-            }
-            
-        ]
-    },
+    }
+    
+
 ];
 
-
 const rest = new REST({ version: '9' }).setToken(TOKEN);
+
+(async () => {
+    try {
+        await rest.put(
+            Routes.applicationCommands(CLIENT_ID),
+            { body: commands },
+        );
+        console.log('コマンドの登録に成功しました！');
+    } catch (error) {
+        console.error('コマンドの登録中にエラーが発生しました:', error);
+    }
+})();
 
 (async () => {
     try {
